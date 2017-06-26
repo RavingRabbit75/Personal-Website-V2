fa4ac31dc532318407a0643e251c11236c01261c
Raymond Chow <ravingrabbit75@gmail.com>
2017-06-25 22:21:41 -0700

    Initial layout work of header.
    Still needs hover states for buttons.

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,17 @@ var cssbeautify = require("gulp-cssbeautify");
 gulp.task("sass", function(){
 	return gulp.src("./scss/styles.scss")
 	.pipe(sass())
+	.on("error", function(error){
+		console.error("\x1b[31m\x1b[1m%s\x1b[0m", "ERROR occured:");
+		console.error("\x1b[35m", error);
+	})
 	.pipe(cssbeautify())
 	.pipe(gulp.dest("./build/css"));
 });
 
 gulp.task("watch", function(){
 	gulp.watch("./scss/*.scss", ["sass"]);
+	gulp.watch("./scss/modules/*.scss", ["sass"]);
 });
 
 gulp.task("default", ["watch", "sass"]);
