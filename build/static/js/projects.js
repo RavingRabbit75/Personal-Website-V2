222ae86a2b6e93134ba8b7db8344b6029c06d942
Raymond Chow <ravingrabbit75@gmail.com>
2017-09-05 21:06:24 -0700

    Started adding the interactivity for changing previews in the project section.

--- build/static/js/projects.js
+++ build/static/js/projects.js
@@ -1,6 +1,6 @@
 $('.block-button').click(function(evt) {
 	evt.preventDefault();
-	console.log(evt.target.dataset.imageidx, evt.target.dataset.prjname);
+	// console.log(evt.target.dataset.imageidx, evt.target.dataset.prjname);
 	url="/api/projects";
 	data={"imageidx":evt.target.dataset.imageidx, "prjname":evt.target.dataset.prjname};
 	$.get({
@@ -11,6 +11,45 @@ $('.block-button').click(function(evt) {
 	});
 
 	function processData(data) {
-		console.log(data);
+		
+	}
+
+	function moveCurrentPreviews() {
+		TweenMax.to(evt.target, 0.25, {opacity:0});
 	}
 });
+
+
+
+var preview1=document.getElementById("preview1");
+var preview2=document.getElementById("preview2");
+
+$('#button1').click(function(evt) {
+	evt.preventDefault();
+	TweenMax.to(preview1, 0.75, {top: "-400px", ease: Power2.easeOut, onComplete: next01});
+});
+
+$('#button2').click(function(evt) {
+	evt.preventDefault();
+	TweenMax.to(preview2, 0.75, {top: "-400px", ease: Power2.easeOut, onComplete: next02});
+});
+
+function next01(){
+	preview1.classList.remove("top");
+	preview1.style.top="0px";
+	preview1.style.boxShadow="0 0 0px rgba(0, 0, 0, .0)";
+	
+	preview1.classList.add("bottom");
+
+	preview2.classList.remove("bottom");
+	preview2.classList.add("top");
+}
+
+function next02(){
+	preview2.classList.remove("top");
+	preview2.style.top="0px";
+	preview2.classList.add("bottom");
+	
+	preview1.classList.remove("bottom");
+	preview1.classList.add("top");
+}
