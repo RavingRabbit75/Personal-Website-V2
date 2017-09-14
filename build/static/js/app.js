83c8d5888c5c1cdadcf18fba3333b45bbcafa104
Raymond Chow <ravingrabbit75@gmail.com>
2017-09-14 13:51:59 -0700

    Switching animation now works for single layout previews in project section.
    The double and triple layouts don't work at the moment.

--- build/static/js/app.js
+++ build/static/js/app.js
@@ -162,6 +162,7 @@ mysite.projects = {
 	},
 
 	setupPreviewButtons: function() {
+		const mq = window.matchMedia( "(min-width: 500px)" );
 
 		$('.block-button-wrapper').click(function(evt) {
 			
@@ -180,18 +181,62 @@ mysite.projects = {
 				// console.log(returnedJSON);
 				console.log(data['prjname']);
 				var hiddenPreviews = $("div[id*='" + data.prjname + "'].bottom").find(".image");
-				
 				for(let x=0; x<hiddenPreviews.length; x++) {
-					console.log(returnedJSON[x].path);
-					console.log(hiddenPreviews[x].src);
-
+					hiddenPreviews[x].src="static/"+returnedJSON[x].path;
 				}
-				// hiddenPreviews[0].src="{{ url_for('static', filename=returnedJSON[0].path) }}";
-				hiddenPreviews[0].src="static/"+returnedJSON[0].path;
+				moveCurrentPreviews(returnedJSON.length);
 			}
 
-			function moveCurrentPreviews() {
-				TweenMax.to(evt.target, 0.25, {opacity:0});
+			function moveCurrentPreviews(layoutNumber) {
+				var layout="";
+				if(layoutNumber===1) {
+					layout="single";
+				} else if (layoutNumber===2) {
+					layout="double";
+				} else if (layoutNumber===3) {
+					layout="triple";
+				}
+				var currentPreviews = $("div[id*='" + data.prjname + "'].top");
+				var currentHeight = $("div[id*='" + data.prjname + "'].top").height();
+				var currentWrapper = $("div[id*='" + data.prjname + "'].top").find(".shadow-on");
+				var hiddenPreviews = $("div[id*='" + data.prjname + "'].bottom");
+				var hiddenWrapper = $("div[id*='" + data.prjname + "'].bottom").find(".shadow-off");
+				var topPosition = currentPreviews.offset().top;
+				// console.log(currentPreviews.offset().top)
+
+				TweenMax.to(currentPreviews, 0.75, {top: "-"+currentHeight+"px"});
+				TweenMax.to(hiddenWrapper, 0.3, {padding: "0px", delay: .7});
+				TweenMax.to(hiddenWrapper, 0.3, {boxShadow: "0 0 5px rgba(0, 0, 0, .4)", delay: 1, onComplete: setClasses});
+
+				// console.log(currentPreviews[0].classList);
+				function setClasses() {
+					currentPreviews[0].classList.remove("top", "previews-row");
+					currentPreviews[0].classList.add("bottom", "previews-row2");
+					console.log(currentPreviews.offset().top);
+					currentWrapper[0].classList.remove("shadow-on");
+					currentWrapper[0].classList.add("shadow-off");
+					currentPreviews.css({top: "5px"});
+					currentWrapper.css({padding: "8px", boxShadow: "0 0 0 rgba(0, 0, 0, .4)"})
+					console.log(currentPreviews.offset().top);
+
+					hiddenPreviews[0].classList.remove("bottom", "previews-row2");
+					hiddenPreviews[0].classList.add("top", "previews-row");
+					hiddenWrapper[0].classList.remove("shadow-off");
+					hiddenWrapper[0].classList.add("shadow-on");
+
+					currentPreviews.insertAfter(hiddenPreviews);
+
+				}
+				
+				// hiddenPreviews
+
+				// if (mq.matches) {
+				//   // window width is at least 500px
+				// } else {
+				//   // window width is less than 500px
+				// }
+				
+				
 			}
 		});
 
